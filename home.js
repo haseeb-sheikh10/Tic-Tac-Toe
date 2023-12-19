700aa1353774e8fbb57956ce2e7bde43c0e780ad
haseeb-sheikh10 <haseeb_irfan1368@hotmail.com>
2023-12-20 02:30:35 +0500

    first commit

--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 let popup = document.querySelector(".popup")
 let input1 = document.querySelector(".input1")
 let input2 = document.querySelector(".input2")
+let error = document.querySelector("#error")
+let okBtn = document.querySelector("#ok")
 
 function openPopup () {
 				popup.classList.add("open-popup");
@@ -18,3 +20,41 @@ function closePopup () {
 
   input1.addEventListener("input", saveValue);
   input2.addEventListener("input", saveValue);
+
+function checkInput1 () {
+				if (input1.value === "") {
+								error1.style.visibility = 'visible';
+				}else if (input1.value !== ""){
+								error1.style.visibility = 'hidden'
+				}
+}
+
+function checkInput2 () {
+				if (input2.value === "") {
+								error2.style.visibility = 'visible';
+				}else if (input2.value !== ""){
+								error2.style.visibility = 'hidden'
+				}
+}
+
+function checkInputs () {
+				if (input1.value !== "" && input2.value !== "") {
+								window.location.href = "index.html"
+				};
+}
+
+okBtn.addEventListener("click", function(){
+				checkInput1();
+				checkInput2();
+				checkInputs();
+})
+
+input1.addEventListener("input", function(){
+				error1.style.visibility = "hidden"
+				error2.style.visibility = "hidden"
+});
+
+input2.addEventListener("input", function(){
+				error2.style.visibility = "hidden"
+				error1.style.visibility = "hidden"
+});
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let boxes = document.querySelectorAll(".box");
-let contentBoxes = document.querySelectorAll(".box-content");
-let resetButton = document.querySelector("#reset");
-let resetButton2 = document.querySelector("#reset2");
-let hideClass = document.querySelector(".hide");
-let player1 = document.querySelector(".player1");
-let player2 = document.querySelector(".player2");
-let result = document.querySelector(".result");
+let boxes = document.querySelectorAll('.box');
+let disBoxes = document.querySelector(".game")
+let resetButton = document.querySelector('#reset');
+let resetButton2 = document.querySelector("#reset2")
+let hideClass = document.querySelector(".hide")
+let player1 = document.querySelector(".player1")
+let player2 = document.querySelector(".player2")
+let result = document.querySelector(".result")
 
 let turnO = true;
 let winPatterns = [
@@ -16,100 +16,108 @@ let winPatterns = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6],
+  [2, 4, 6]
 ];
-boxes.forEach((box, index) => {
-  box.addEventListener("click", () => {
-    if (contentBoxes[index].textContent === "") {
+boxes.forEach(box => {
+  box.addEventListener('click', () => {
+    if (box.textContent === '') {
       if (turnO) {
-        contentBoxes[index].textContent = "O";
+        box.textContent = 'O';
         turnO = false;
       } else {
-        contentBoxes[index].textContent = "X";
+        box.textContent = 'X';
         turnO = true;
+      
       }
-      updatePlayers();
+      updatePlayers()
       checkWin();
+      
     }
   });
 });
 
-let disBoxes = () => {
-  for (let box of boxes) {
-    box.disabled = true;
-  }
-};
 
-let p1 = "O";
-let p2 = "X";
+
+let p1 = "O"
+let p2 = "X"
+let result1 = localStorage.getItem("input1Value")
+let result2 = localStorage.getItem("input2Value")
 
 let showWinner = (winner) => {
-  hideClass.classList.add("openGameOver");
-  if (winner === p1) {
-    result.textContent = player1.textContent + " wins!";
-  } else {
-    result.textContent = player2.textContent + " wins!";
-  }
-
-  disBoxes();
-};
+   hideClass.classList.add("openGameOver")
+   if (winner === p1) {
+   				result.textContent = result1 + (" wins!")
+   }else {
+   				result.textContent = result2 + (" wins!")
+   }
+   player1.style.visibility = "hidden"
+   player2.style.visibility = "hidden"
+  disBoxes.classList.add("dis-boxes")
+}
 
 let checkWin = () => {
-  let index = 0;
+
+  let index = 0
   let tie = true;
   for (let pattern of winPatterns) {
-    let first = contentBoxes[pattern[0]].textContent;
-    let second = contentBoxes[pattern[1]].textContent;
-    let third = contentBoxes[pattern[2]].textContent;
-    index++;
-    if (first != "" && second != "" && third != "") {
-      if (first === second && second === third) {
-        showWinner(first);
-      } else {
-        for (let box of contentBoxes) {
-          if (box.textContent === "") {
-            tie = false;
-            break;
-          }
-        }
-
-        if (tie && index === 8) {
-          hideClass.classList.add("openGameOver");
-          result.textContent = "Draw!";
-          disBoxes();
-        }
+    let first = boxes[pattern[0]].textContent
+    let second = boxes[pattern[1]].textContent
+    let third = boxes[pattern[2]].textContent 
+index++;
+    if (first != "" && second != "" && third != "" ) {
+      
+    if (first === second && second === third ) {
+    
+    showWinner(first);
+  } else  {
+      for (let box of boxes) {
+        if (box.textContent === "") {
+           tie = false;
+           break;
+        };
       }
+      
+      if (tie && index === 8) {
+        hideClass.classList.add("openGameOver")
+        result.textContent = "Draw!";
+        disBoxes.classList.add("dis-boxes")
+      
     }
+   }
   }
-};
+}
+}
 
 const resetFunc = () => {
-  hideClass.classList.remove("openGameOver");
-  contentBoxes.forEach((box) => {
-    box.textContent = "";
-    box.disabled = false;
+  hideClass.classList.remove('openGameOver')
+  player1.style.visibility = "visible"
+  player2.style.visibility = "visible"
+  boxes.forEach(box => {
+    box.textContent = '';
+    disBoxes.classList.remove("dis-boxes")
   });
   updatePlayers();
-};
+  };
 
-resetButton.addEventListener("click", resetFunc);
+resetButton.addEventListener('click', resetFunc);
 
-resetButton2.addEventListener("click", resetFunc);
+resetButton2.addEventListener('click', resetFunc);
 
-function updatePlayers() {
-  rightTurn();
-  player1.textContent = localStorage.getItem("input1Value") + "'s turn";
-  player2.textContent = localStorage.getItem("input2Value") + "'s turn";
-}
+  function updatePlayers() {
+  rightTurn()
+    player1.textContent = localStorage.getItem("input1Value") + "'s turn";
+    player2.textContent = localStorage.getItem("input2Value") + "'s turn";
+  }
 
-window.addEventListener("DOMContentLoaded", updatePlayers);
+  window.addEventListener("DOMContentLoaded", updatePlayers);
 
 function rightTurn() {
-  if (turnO === true) {
-    player2.classList.add("rightTurn");
-    player1.classList.remove("rightTurn");
-  } else if (turnO !== true) {
-    player1.classList.add("rightTurn");
-    player2.classList.remove("rightTurn");
-  }
+				if (turnO === true) {
+								player2.classList.add("rightTurn")
+								player1.classList.remove("rightTurn")
+				}else if (turnO !== true){
+				player1.classList.add("rightTurn")
+				player2.classList.remove("rightTurn")
 }
+}
+				
